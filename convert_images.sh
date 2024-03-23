dcc2d0c21adbe55b6c4a46d1eba89b3e4341ce87
folguinch <f.olguin.ch@gmail.com>
2024-03-23 08:45:22 +0000

    Deploying to gh-pages from @ folguinch/website@5ce3cd252b364307d8349f1b65a67602751e7029 🚀

---
+++ convert_images.sh
@@ -0,0 +1,2 @@
+convert /mnt/Aether/Pictures/2023/07_Carretera_Austral/DSC_1676.jpg -quality 95 -resize 1400 -auto-orient assets/img/carretera_austral_020.jpg
+convert /mnt/Aether/Pictures/mobile/asus/Camera/P_20231014_130419.jpg -quality 95 -resize 1400 -auto-orient assets/img/carretera_austral_036.jpg
